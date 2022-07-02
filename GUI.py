af98b60a411e1adbdc38d21535d02ce29dd29e7d
iliodium <95029175+iliodium@users.noreply.github.com>
2022-07-02 20:05:26 +0300

    Окно
    Сделал окно и подгрузку kv файла

--- GUI.py
+++ GUI.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+
+
+class TAS(MDApp):
+    def build(self):
+        '''Загрука файла GUI.kv'''
+        return Builder.load_file("GUI.kv")
+
+
+if __name__ == '__main__':
+    TAS().run()
